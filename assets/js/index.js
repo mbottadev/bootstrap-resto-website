5634cbcf3fd0ef291d20f9681ac1a53b51d46334
MaxBot <maxime.botta.dev@gmail.com>
2019-02-26 15:32:48 +0100

    first push, all 5 pages created

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+
+    let logo = document.getElementById("logo")
+
+    
+    // logo.addEventListener("mouseenter", function(e){
+    //     e.target.classList.add = "animated";
+    //     e.target.style.opacity = 0.4;
+    //     e.target.style.backgroundColor = "white";
+    //     e.target.style.borderRadius = 10 + "px";
+        
+
+    // })
+    // logo.addEventListener("mouseleave", function(e){
+    //     e.target.classList.remove = "animated";
+    //     e.target.style.opacity = 1;
+    //     e.target.style.backgroundColor = " ";
+    //     e.target.style.borderRadius = "";
+
+
+
+
+    // })
+
+}
