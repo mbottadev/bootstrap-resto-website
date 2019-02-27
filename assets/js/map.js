e29910a7d7bd3324de65f523abc9b0d07d513ef4
MaxBot <maxime.botta.dev@gmail.com>
2019-02-27 16:37:55 +0100

    end of wednesday work, creation of contact, and photos

---
+++ assets/js/map.js
@@ -0,0 +1,35 @@
+// window.onload = () => {
+
+//     // let jumpBtn = document.querySelector(".jumpBtn")
+//     // let jumpBtn = document.querySelector(".jumpBtn")
+//     // let jumpBtn = document.querySelector(".jumpBtn")
+//     // let jumpBtn = document.querySelector(".jumpBtn")
+//     // let jumpDiv = document.querySelector(".jumpDiv")
+
+
+    
+//     // jumpBtn.addEventListener("mouseenter", function(e){
+//     //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+//     //     e.target.style.color = "white";
+
+
+//     // })
+
+//     // jumpBtn.addEventListener("mouseleave", function(e){
+//     //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+
+//     // })
+
+//     <span class="metadata-marker" style="display: none;" data-region_tag="html-body"></span>            <div id="map"></div>
+//     <script>
+//     <span class="metadata-marker" style="display: none;" data-region_tag="script-body"></span>      var map;
+//         function initMap() {
+//             map = new google.maps.Map(document.getElementById('map'), {
+//             center: {lat: -34.397, lng: 150.644},
+//             zoom: 8
+//             });
+//         }
+//     </script>
+//     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQDvS4k0JwzYO6anX6ckLtZutThxnpqBo&callback=initMap"
+//     async defer></script>
+// }
