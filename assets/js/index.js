3fa1a56ddfb87aa0c77a57db71b19735acd3ef8b
MaxBot <maxime.botta.dev@gmail.com>
2019-03-01 16:29:38 +0100

    final project v2 sorry Bertrand

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,3 @@
 window.onload = () => {
 
-    // var rep = prompt ('What is your name?')
-
-    // name = document.createElement('div');
-    // name.id = "namePrompt"
-    // document.getElementById('popUp').innerText = "Hello " + rep + " welcome to"
-    // document.getElementsByTagName('body')[0].appendChild(name);
-
-    // logo.addEventListener("mouseenter", function(e){
-    //     e.target.classList.add = "animated";
-    //     e.target.style.opacity = 0.4;
-    //     e.target.style.backgroundColor = "white";
-    //     e.target.style.borderRadius = 10 + "px";
-        
-
-    // })
-    // logo.addEventListener("mouseleave", function(e){
-    //     e.target.classList.remove = "animated";
-    //     e.target.style.opacity = 1;
-    //     e.target.style.backgroundColor = " ";
-    //     e.target.style.borderRadius = "";
-
-
-
-
-    // })
-
 }
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,84 +1,50 @@
-// window.onload = () => {
-
-    // let jumpBtn = document.querySelector(".jumpBtn")
-    // let jumpBtn = document.querySelector(".jumpBtn")
-    // let jumpBtn = document.querySelector(".jumpBtn")
-    // let jumpBtn = document.querySelector(".jumpBtn")
-    // let jumpDiv = document.querySelector(".jumpDiv")
-
-
+function getDate(date){
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
     
-    // jumpBtn.addEventListener("mouseenter", function(e){
-    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
-    //     e.target.style.color = "white";
-
-
-    // })
-
-    // jumpBtn.addEventListener("mouseleave", function(e){
-    //     e.target.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
-
-    // })
-
-    // function initMap() {
-    //     map = new google.maps.Map(document.getElementById('map'), {
-    //         center: {lat: -34.397, lng: 150.644},
-    //         zoom: 8
-    //     });
-    // }
-
-    function getDate(date){
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var hour = date.getHours();
-        var minutes = date.getMinutes();
-        
-            if (day < 10) {
-                day = "0" + day;
-            }
-            if (month < 10) {
-                month = "0" + month;
-            }
-            return day + "-" + month + "-" + year + " at " + hour + "H" + minutes;
-            };
-        
-                function checkIfOpen(date) {
-                    var closedDays= ['Monday']
-                    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-                    var day = date.getDay();
-                    if (closedDays.includes(days[day])) {
-                        return 'closed'
-                    } else {
-                        return 'open'
-                    }
-                }
-        
-        function setDate() {
-            elem = document.getElementById("date");
-                    const date = new Date();
-                    elem.innerText = "Today it's the " + getDate(date) + ", our restaurants are " + checkIfOpen(date);
-                    console.log(date)
+        if (day < 10) {
+            day = "0" + day;
         }
-        
-        window.onload = () => {
-            setDate();
+        if (month < 10) {
+            month = "0" + month;
+        }
+        return day + "-" + month + "-" + year + " at " + hour + "H" + minutes;
         };
-
-    function initMap() {
-        // The location of bourse
-        let stockel = {lat: 50.8410059, lng:4.4635135};
-        let merode = {lat: 50.8392854, lng:4.396738};
-        let bourse = {lat: 50.8475812, lng:4.3472005};
-        // The map, centered at bourse
-        let map = new google.maps.Map(
-            document.getElementById('map'), {zoom: 12, center: bourse});
-        // The marker, positioned at bourse
-        let marker1 = new google.maps.Marker({position: stockel, map: map});
-        let marker2 = new google.maps.Marker({position: merode, map: map});
-        let marker3 = new google.maps.Marker({position: bourse, map: map});
+    
+            function checkIfOpen(date) {
+                var closedDays= ['Monday']
+                var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                var day = date.getDay();
+                if (closedDays.includes(days[day])) {
+                    return 'closed'
+                } else {
+                    return 'open'
+                }
+            }
+    
+    function setDate() {
+        elem = document.getElementById("date");
+                const date = new Date();
+                elem.innerText = "Today it's the " + getDate(date) + ", our restaurants are " + checkIfOpen(date);
+                console.log(date)
     }
+    
+    window.onload = () => {
+        setDate();
+    };
 
-    
-    
-// }
+function initMap() {
+    let stockel = {lat: 50.8410059, lng:4.4635135};
+    let merode = {lat: 50.8392854, lng:4.396738};
+    let bourse = {lat: 50.8475812, lng:4.3472005};
+
+    let map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 12, center: bourse});
+
+    let marker1 = new google.maps.Marker({position: stockel, map: map});
+    let marker2 = new google.maps.Marker({position: merode, map: map});
+    let marker3 = new google.maps.Marker({position: bourse, map: map});
+}
